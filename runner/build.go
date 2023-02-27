d2aab302e08eaab31e157fc6117350c62e3a7aec
chinesewhiteboi <douglasboyle209@gmail.com>
2023-02-27 01:13:54 +0000

    go runner/build: add go generate

--- runner/build.go
+++ runner/build.go
@@ -7,8 +7,42 @@ import (
 	"os/exec"
 )
 
+func generate() (string, bool) {
+	cmd := exec.Command("go", "generate", "./...")
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		fatal(err)
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fatal(err)
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		fatal(err)
+	}
+
+	io.Copy(os.Stdout, stdout)
+	errBuf, _ := ioutil.ReadAll(stderr)
+
+	err = cmd.Wait()
+	if err != nil {
+		return string(errBuf), false
+	}
+
+	return "", true
+}
+
 func build() (string, bool) {
 	buildLog("Building...")
+
+	output, ok := generate()
+	if !ok {
+		return output, false
+	}
 
 	cmd := exec.Command("go", "build", "-o", buildPath(), root())
 
